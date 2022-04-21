489f72e469d03f25ba1aa25e69e3ae7d04ef38c7
zacwastaken <doicirohangnan@gmail.com>
2022-04-21 08:44:33 -0500

    data: begun codifyng data into excel

--- scripts/ProcessData.R
+++ scripts/ProcessData.R
@@ -1,3 +1,5 @@
+# Package dependencies 
+
 library(tidyverse)
 library(lubridate)
 library(xts)
@@ -17,5 +19,15 @@ library(lattice)
 library(descr)
 library(openxlsx)
 
+##### Shame doc: everything we don't know where to put but we still need to remember
+###### The other games: Wild Rift, Genshin Impact, Brawl Stars, Dofus, Clash of Clans, Mobile Legends, Parchis
+
 dataf <- read_excel("data/raw/Answers.xlsx")
 View(dataf)
+
+attach(dataf)
+
+gender <- factor(c(dataf$`¿Cuál es su género?`))
+gender
+gender_unclass <- unclass(gender)
+gender_unclass
